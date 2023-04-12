08670168ada8da1ad74a70203f64cca64db56384
ContiClara <estudiante@UCUXA60633.continental.edu.pe>
2023-04-12 17:18:44 -0500

    Trabajo

---
+++ operaciones.py
@@ -0,0 +1,14 @@
+#insertar dos numeros dos numeros y calcular la suma , resta , multiplicacion ,y division 
+print ("ingrese el primer numero:")
+num1 = float (input())
+print ("ingrese el segundo  numero:")
+num2 = float (input())
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2 
+print ("la suma es:", suma )
+print ("la resta es:", resta )
+print ("la multiplicacion  es:", multiplicacion )
+print ("la division  es:", division  )
+
