e4b20a3ba5415eb200f0c2e5e3a510c7ae3bf5cc
lourdesconti <estudiante@UCUXA60632.continental.edu.pe>
2023-04-12 17:29:01 -0500

    modifique la linea 10

--- operaciones.py
+++ operaciones.py
@@ -7,7 +7,7 @@ suma = num1 + num2
 resta = num1 - num2
 multiplicacion = num1 * num2
 division = num1 / num2 
-print ("la suma es:", suma )
+print ("la suma es4:", suma )
 print ("la resta es:", resta )
 print ("la multiplicacion  es:", multiplicacion )
 print ("la division  es:", division  )
